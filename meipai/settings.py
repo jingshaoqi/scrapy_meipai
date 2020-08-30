bcbe6abb1201ecf37ec92392a1c0c9d1789c7561
chencichang <cichang.chen@verycloud.cn>
2020-08-30 22:12:04 +0800

    添加注释

--- meipai/settings.py
+++ meipai/settings.py
@@ -16,8 +16,9 @@ NEWSPIDER_MODULE = 'meipai.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
-
+#USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36"
+#USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36"
+USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36'
 # Obey robots.txt rules
 # ROBOTSTXT_OBEY = True
 
--- meipai/spiders/meipaispider.py
+++ meipai/spiders/meipaispider.py
@@ -1,6 +1,8 @@
+# 美拍的视频地址进行了加密，先暂停，以后再来研究
 import scrapy
 import uuid
 import time
+from urllib.parse import urljoin
 from scrapy.spiders import CrawlSpider, Rule, Spider
 from meipai.items import MeipaiItem
 from selenium import webdriver
@@ -12,17 +14,18 @@ class MeipaiSpider(Spider):
     allowed_domains = ['www.meipai.com', 'mvvideo10.meitudata.com', 'mvvideo11.meitudata.com', ]
     # url = 'http://www.tu11.com/xingganmeinvxiezhen/list_1_'
     # offset = 1
+    # crawl meipai hot video
     start_urls = ['http://www.meipai.com/medias/hot', ]
 
     def parse(self, response):
         items = response.xpath('//ul[@id="mediasList"]/li/div/a[@class="content-l-p pa"]')
         item_list = items[:1]
         for i in item_list:
-
             video_link = i.xpath('.//@href').extract()[0]
-            # http: // www.meipai.com / media / 1039574971
-            yield scrapy.Request('http://www.meipai.com' + video_link, callback=self.content)
-
+            # http://www.meipai.com/media/1039574971
+            video_link_full_link = urljoin(response.url, video_link)
+            yield scrapy.Request(url=video_link_full_link, callback=self.content)
+            break
 
         # next_page = response.xpath('//dl[@class="list-left public-box"]/dd[@class="page"]//a[last()-1]/text()')
         # if "下一页" == next_page.extract()[0]:
@@ -30,6 +33,21 @@ class MeipaiSpider(Spider):
         #     yield response.follow(next_page_url, callback=self.parse)
 
     def content(self, response):
+        with open('res.html','w') as f:
+            f.write(response.text)
+        # 现在的美拍的视频url地址是经过加密的，可以查看
+        # https://www.jianshu.com/p/446e57544f57
+        # https://www.52pojie.cn/thread-1074085-1-1.html
+        # 中的方法
+        # 执行的函数为 playPicsVideo
+        video_src = response.xpath('//div[@class="mp-h5-player-layer-video"]')
+        print(video_src)
+        url = urljoin(response.url, video_src)
+        name = time.localtime()
+        item = MeipaiItem()
+        item['url'] = url
+        yield item
+        return
 
         chrome_options = Options()
 
